dcf495c2fdf432e337ad1d69a6e8b142664024da
alexanderceoh <alexanderceoh@gmail.com>
2015-10-08 17:31:17 -0400

    break paddles and bricks

---
+++ Break/Break/StartViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StartViewController.swift
+//  Break
+//
+//  Created by Alex Oh on 10/8/15.
+//  Copyright © 2015 Alex Oh. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        let gameVC = GameViewController()
+        
+        // navigation controller has an array of viewControllers, everytime we push an object we are adding it to the array
+        // gameViewController is replacing the default startViewController
+        // there's no reason to go back to StartViewController
+        navigationController?.viewControllers = [gameVC]
+        
+    }
+    
+}
